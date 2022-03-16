568a308fb74e3146d76a7ff1643aec9791aec5dc
feLycks <felix.obiero@student.moringaschool.com>
2022-03-16 16:03:44 +0300

    routing

--- awards/urls.py
+++ awards/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('awardsclone/', include('awardsclone.urls')),
+    path('', include('awardsclone.urls')),
 ]
