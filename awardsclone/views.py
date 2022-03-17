bf486a410500713960f2d3007616fe9699557b39
feLycks <felix.obiero@student.moringaschool.com>
2022-03-17 03:34:34 +0300

    routing and template inheritance

--- awardsclone/views.py
+++ awardsclone/views.py
@@ -1,5 +1,23 @@
 
 from django.shortcuts import render, HttpResponse
 
+posts = [
+    {
+        'author' : 'Paul Walker', 
+        'title' : 'Making it look right ', 
+        'content' : 'Lorem ipsum finds people unawre',
+        'date_posted' : 'August 20th, 2000'
+    }, 
+    {
+        'author' : 'jane Doe', 
+        'title' : 'Second Post',
+        'content' : 'I wish things would be better',
+        'date_posted' : 'May 16th, 2030'
+    }
+]
+
 def home(request):
-    return render (request, 'index.html')
+    context = {
+        'posts' : posts
+    }
+    return render (request, 'index.html', context)
